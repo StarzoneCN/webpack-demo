7212cbe5f338de65ffbcb8d28c1a3d417793ca83
LiHongxing <lihongxing@bluemoon.com.cn>
2018-04-12 13:54:56 +0800

    代码精简工具：删除bundle.js中没有被引用的export（精简的结果和教程有些出入，但是确实是精简了）

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, 'dist'),
         publicPath: "dist/"
-    }
+    },
+    plugins: [
+        new UglifyJSPlugin()
+    ]
 }
