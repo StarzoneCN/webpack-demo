c4ce3dfeb2d57ac42a0afee6b231e1ace8f9a738
LiHongxing <lihongxing@bluemoon.com.cn>
2018-04-13 10:24:32 +0800

    bundle文件名添加hash标记； build之前清理dist目录

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const cleanWebpack = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: "[name].bundle.js",
+        filename: "[name].[chunkhash].bundle.js",
         path: path.resolve(__dirname, 'dist'),
-        chunkFilename: '[name].bundle.js'
+        chunkFilename: '[name].[chunkhash].bundle.js'
     },
     plugins: [
+        new cleanWebpack(['dist']),
         new HTMLWebpackPlugin({
-            title: 'Code Splitting'
+            title: 'Caching'
         })
     ]
 }
